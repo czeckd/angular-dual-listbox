e377084b04e9b39fce1528d994eef2d603958965
David Czeck <david@ibiblio.org>
2017-04-22 13:34:22 -0700

    Add FormsModule

--- lib/angular-dual-listbox.module.ts
+++ lib/angular-dual-listbox.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { DualListComponent } from './dual-list.component';
 
 @NgModule({
-	imports:      [ CommonModule ],
+	imports:      [
+		CommonModule,
+		FormsModule
+	],
 	declarations: [ DualListComponent ],
 	exports:      [ DualListComponent ]
 })
