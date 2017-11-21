9f3d9dafca78e299b9ea2d66194ad131b97d8fd9
ekwebster <33794614+ekwebster@users.noreply.github.com>
2017-11-20 19:51:09 -0600

    Update angular-dual-listbox.module.ts

--- lib/angular-dual-listbox.module.ts
+++ lib/angular-dual-listbox.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { DualListComponent } from './dual-list.component';
 
 @NgModule({
 	imports:      [
 		CommonModule,
-		FormsModule
+		FormsModule,
+		NgbModule
 	],
 	declarations: [ DualListComponent ],
 	exports:      [ DualListComponent ]
