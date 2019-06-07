c3162bdc1feeafe85fb8f32731b87b0635138e3e
David Czeck <david@ibiblio.org>
2019-06-07 02:15:42 -0700

    Migrate to Angular 8 and Angular CLI

---
+++ projects/angular-dual-listbox/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of angular-dual-listbox
+ */
+
+export * from './lib/basic-list';
+export * from './lib/dual-list.component';
+export * from './lib/angular-dual-listbox.module';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { DualListComponent } from 'angular-dual-listbox';
 import { DemoAppComponent } from './demo-app.component';
